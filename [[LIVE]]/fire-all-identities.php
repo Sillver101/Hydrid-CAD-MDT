6d37a5d692508f9e730ca477c44b23c8853971a6
Justin Farner <31464982+justinfarmer14@users.noreply.github.com>
2018-11-15 08:23:20 -0600

    Revert "Add files via upload"
    This reverts commit 677afdd9a4520de80f8943a43fbcc228bdcb74d9.

--- [[LIVE]]/fire-all-identities.php
+++
@@ -1,3 +0,0 @@
-<?php
-require 'classes/connect.php';
-require '../main-core/fire-all-identities.php';
--- [[LIVE]]/staff-edituser.php
+++
@@ -1,3 +0,0 @@
-<?php
-require 'classes/connect.php';
-require '../main-core/staff-edituser.php';
